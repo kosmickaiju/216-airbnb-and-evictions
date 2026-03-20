9c44b363d7ba10bfa80b91d1347a91c577c18f03
Luca Laureno <luca.laureno@gmail.com>
2026-03-20 13:46:06 -0400

    setting up code structure, cleaning data

--- analysis.py
+++ analysis.py
@@ -10,4 +10,15 @@ print(listings.head())
 evictions = pd.read_csv("evictions.csv", index_col=0)
 print(evictions.head())
 
-#we should try to analyze by borough - both datasets have boroughs but we need to clean the data so that they align
+
+'''we should try to analyze by borough - both datasets have boroughs but we need to clean the data so that they align. so like number of evictions within each borough and see if the density of airbnbs in each borough goes up w eviction rates'''
+#calculations
+brooklyn_count = listings['neighbourhood_group'].value_counts()['Brooklyn']
+manhattan_count = listings['neighbourhood_group'].value_counts()['Manhattan']
+queens_count = listings['neighbourhood_group'].value_counts()['Queens']
+bronx_count = listings['neighbourhood_group'].value_counts()['Bronx']
+staten_count = listings['neighbourhood_group'].value_counts()['Staten Island'] #or could just add new column in listings csv but idk it feels weird
+
+#visualizations
+
+#data analysis/statistical significance
