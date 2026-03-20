0cd6fc97ac5ca204a728e9c82d37a42d6065eb6a
Luca Laureno <luca.laureno@gmail.com>
2026-03-20 16:43:07 -0400

    cleaned up data and calculated needed values

--- analysis.py
+++ analysis.py
@@ -2,22 +2,28 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
 
 #reading datasets into dataframes
-listings = pd.read_csv("listings.csv", index_col=0)
-listings.drop(columns=['price'], inplace=True) #the entire column is blank for some reason so i think we should drop it
+listings = pd.read_csv("listings.csv")
 print(listings.head())
-evictions = pd.read_csv("evictions.csv", index_col=0)
+evictions = pd.read_csv("evictions.csv")
 print(evictions.head())
 
 
-'''we should try to analyze by borough - both datasets have boroughs but we need to clean the data so that they align. so like number of evictions within each borough and see if the density of airbnbs in each borough goes up w eviction rates'''
-#calculations
-brooklyn_count = listings['neighbourhood_group'].value_counts()['Brooklyn']
-manhattan_count = listings['neighbourhood_group'].value_counts()['Manhattan']
-queens_count = listings['neighbourhood_group'].value_counts()['Queens']
-bronx_count = listings['neighbourhood_group'].value_counts()['Bronx']
-staten_count = listings['neighbourhood_group'].value_counts()['Staten Island'] #or could just add new column in listings csv but idk it feels weird
+'''we should try to analyze by borough bc there are way too many individual neighborhoods in nyc - both datasets have boroughs but we need to clean the data so that they align. so like number of evictions within each borough and see if the density of airbnbs in each borough goes up w eviction rates'''
+#data wrangling
+evictions = evictions[evictions['Residential/Commercial'] == 'Residential'].copy() #residential evictions only
+evictions['BOROUGH'] = evictions['BOROUGH'].str.title()
+
+listings.drop(columns=['price', 'license', 'host_profile_id']) #these columns are empty and/or irrelevant
+listings['is_entire_home'] = listings['room_type'] == 'Entire home/apt'
+
+eviction_counts = (evictions[evictions['Residential/Commercial'] == 'Residential'].groupby('BOROUGH').size().reset_index(name='eviction_count'))
+listing_agg = (listings.groupby('neighbourhood_group').agg(total_listings = ('id', 'count'), entire_home_count = ('is_entire_home', 'sum')).reset_index())
+listing_agg['entire_home_proportion'] = (listing_agg['entire_home_count'] / listing_agg['total_listings'])
+merged = pd.merge(eviction_counts, listing_agg, left_on='BOROUGH', right_on='neighbourhood_group', how='inner') #merging dataset borough columns
 
 #visualizations
 
